2e47f798b40fc2a20853aedc4dda46072bd44572
Aayushi Parekh <aayushiparekh2812@gmail.com>
2024-09-04 22:31:53 -0400

    Initial Commit

---
+++ itprepmate/src/main/java/com/example/itprepmate/ItprepmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.itprepmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItprepmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItprepmateApplication.class, args);
+	}
+
+}
